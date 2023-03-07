bee98a385ee30ac30bff2ff293eff8abec4c30e8
Superkat32 <superkat1806@gmail.com>
2023-03-06 20:07:23 -0500

    New Hampter goals
    FEAT: Hampter will now attack more mobs, which include but are not limited to Iron Golems, Pillagers, Evokers, Snow Golems, and the Warden.

--- src/main/java/net/superkat/hampter/entity/custom/HampterEntity.java
+++ src/main/java/net/superkat/hampter/entity/custom/HampterEntity.java
@@ -6,14 +6,16 @@ import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.mob.HostileEntity;
-import net.minecraft.entity.mob.SpiderEntity;
+import net.minecraft.entity.mob.*;
+import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.SnowGolemEntity;
 import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -39,14 +41,32 @@ public class HampterEntity extends HostileEntity implements IAnimatable {
 
     @Override
     protected void initGoals() {
+        //Movement
         this.goalSelector.add(1, new SwimGoal(this));
+        this.goalSelector.add(2, new BreatheAirGoal(this));
         this.goalSelector.add(2, new MeleeAttackGoal(this, 1.2D, false));
+        this.goalSelector.add(3, new RevengeGoal(this));
         this.goalSelector.add(3, new WanderAroundFarGoal(this, 0.75f, 1));
         this.goalSelector.add(4, new LookAroundGoal(this));
 
-        this.targetSelector.add(3, new ActiveTargetGoal<>(this, SpiderEntity.class, true));
+        //Monsters
+        this.targetSelector.add(2, new ActiveTargetGoal<>(this, SpiderEntity.class, true));
+        this.targetSelector.add(5, new ActiveTargetGoal<>(this, WardenEntity.class, true));
+
+        //Villagers
         this.targetSelector.add(3, new ActiveTargetGoal<>(this, MerchantEntity.class, true));
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, IronGolemEntity.class, true));
         this.targetSelector.add(4, new ActiveTargetGoal<>(this, WanderingTraderEntity.class, true));
+
+        //Pillagers
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, PillagerEntity.class, true));
+        this.targetSelector.add(2, new ActiveTargetGoal<>(this, VindicatorEntity.class, true));
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, EvokerEntity.class, true));
+        this.targetSelector.add(1, new ActiveTargetGoal<>(this, VexEntity.class, true));
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, IllusionerEntity.class, true));
+
+        //Other
+        this.targetSelector.add(4, new ActiveTargetGoal<>(this, SnowGolemEntity.class, true));
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
@@ -59,10 +79,22 @@ public class HampterEntity extends HostileEntity implements IAnimatable {
         return PlayState.CONTINUE;
     }
 
+    private PlayState attackPredicate(AnimationEvent event) {
+        if(this.handSwinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
+            event.getController().markNeedsReload();
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.hampter.attack", false));
+            this.handSwinging = false;
+        }
+
+        return PlayState.CONTINUE;
+    }
+
     @Override
     public void registerControllers(AnimationData animationData) {
         animationData.addAnimationController(new AnimationController(this, "controller",
                 0, this::predicate));
+        animationData.addAnimationController(new AnimationController(this, "attackController",
+                0, this::attackPredicate));
     }
 
     @Override
